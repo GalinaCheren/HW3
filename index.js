23143d539da73771e834f6efef478e71a540cc4f
GalinaCheren <galinacheren8@gmail.com>
2022-10-05 20:38:06 +0300

    Comment console

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let fistNumber = prompt("What is the first number?", "");
 let secondNumber = prompt("What is the second number?", "");
 
 alert(Number(fistNumber) + Number(secondNumber));
-console.log(+fistNumber + +secondNumber);
+// console.log(+fistNumber + +secondNumber);
