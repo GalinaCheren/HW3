20ec07d93b682d0d6ceb373640f05312b35d2957
GalinaCheren <galinacheren8@gmail.com>
2022-10-05 20:26:41 +0300

    Home Work 3

---
+++ index.js
@@ -0,0 +1,5 @@
+let fistNumber = prompt("What is the first number?", "");
+let secondNumber = prompt("What is the second number?", "");
+
+alert(Number(fistNumber) + Number(secondNumber));
+console.log(+fistNumber + +secondNumber);
